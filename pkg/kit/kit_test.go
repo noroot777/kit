aa7db528cebeb5e3e3a42268c6eab4671db074aa
noroot777 <fandayrockworld@gmail.com>
2020-12-15 11:20:39 +0800

    describe test

--- pkg/kit/kit_test.go
+++ pkg/kit/kit_test.go
@@ -1,5 +1,16 @@
 package kit
 
+import (
+	"fmt"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/cli-runtime/pkg/resource"
+	"k8s.io/kubectl/pkg/describe"
+)
+
 // Watch s
 // func TestWatch(t *testing.T) {
 // 	config, _ := clientcmd.BuildConfigFromFlags("", "./test.kubeconfig")
@@ -22,3 +33,57 @@ package kit
 // 		}
 // 	}
 // }
+
+func TestIdea(t *testing.T) {
+	getter := genericclioptions.NewConfigFlags(false)
+
+	var Describer func(*meta.RESTMapping) (describe.ResourceDescriber, error) = func(mapping *meta.RESTMapping) (describe.ResourceDescriber, error) {
+		return describe.DescriberFn(getter, mapping)
+	}
+	// config, _ := clientcmd.BuildConfigFromFlags("", "./test.kubeconfig")
+	b := *resource.NewBuilder(getter)
+	r := b.
+		Unstructured().
+		ContinueOnError().
+		NamespaceParam("imds").DefaultNamespace().AllNamespaces(false).
+		// FilenameParam(o.EnforceNamespace, o.FilenameOptions).
+		LabelSelectorParam("").
+		ResourceTypeOrNameArgs(true, "event", "imds-web-764f9c4bb8.1650c4ade4451b20").
+		// Flatten().
+		Do()
+	// fmt.Printf("err: %v\n", r.Err())
+	// fmt.Printf("r.mapper: %+v\n", r.Mapper())
+	infos, _ := r.Infos()
+	// fmt.Printf("r: %+v\n", infos)
+	// fmt.Printf("r: %+v\n", infos[0].Object)
+	errs := sets.NewString()
+	first := true
+
+	for _, info := range infos {
+		mapping := info.ResourceMapping()
+		describer, err := Describer(mapping)
+		if err != nil {
+			if errs.Has(err.Error()) {
+				continue
+			}
+			// allErrs = append(allErrs, err)
+			errs.Insert(err.Error())
+			continue
+		}
+		s, err := describer.Describe(info.Namespace, info.Name, describe.DescriberSettings{ShowEvents: false})
+		if err != nil {
+			if errs.Has(err.Error()) {
+				continue
+			}
+			// allErrs = append(allErrs, err)
+			errs.Insert(err.Error())
+			continue
+		}
+		if first {
+			first = false
+			fmt.Printf(s)
+		} else {
+			fmt.Printf("\n\n%s", s)
+		}
+	}
+}
